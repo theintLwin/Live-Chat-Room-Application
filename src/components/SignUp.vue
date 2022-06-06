2fb12ce0a090b0f17891d49c9b2737c6227fb9c8
theintLwin <theinttheintlwin38@gmail.com>
2022-06-06 11:57:46 +0630

    refactoring our sign up accout (useSignup composables)

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -10,32 +10,21 @@
 
 <script>
 import { ref } from '@vue/reactivity';
-import {auth} from '../firebase/config'
+
+import useSignup from '../composables/useSignup'
 export default {
     setup(){
       let displayName = ref("");
       let email = ref("");
       let password = ref("");
-      let error = ref(null);
+      
+      let {error, createAccount}=useSignup();
+
       let signUp = async()=>{
          // console.log(displayName.value, email.value, password.value);
-        try{
-          let res=await auth.createUserWithEmailAndPassword(email.value,password.value);
-       
-          if(!res){
-            throw new Error("could not create new user");
+        let res=await createAccount(email.value, password.value, displayName.value);
+        console.log(res.user)
           }
-         res.user.updateProfile({displayName:displayName.value});
-          console.log(res.user);
-
-
-        }catch(err){
-          
-          error.value=err.message;
-          console.log(error.value);
-        }
-
-      }
 
       return {displayName, email, password, signUp, error}
     }
