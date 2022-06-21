49706f2e2062826aa6ca6b79900485652dfe6277
theintLwin <theinttheintlwin38@gmail.com>
2022-06-21 10:50:56 +0630

    change our date format and auto scrolling feature

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="chat-window">
-    <div class="messages" >
-        <div class="single" v-for="message in messages" :key="message.id">
-            <span class="created-at">{{message.created_at.toDate()}}</span>
+    <div class="messages" ref="msgBox">
+        <div class="single" v-for="message in formatttedMessages" :key="message.id">
+            <span class="created-at">{{message.created_at}}</span>
             <span class="name">{{message.name}}</span>
             <span class="message">{{message.message}}</span>
         </div>
@@ -13,9 +13,26 @@
 <script>
 import { db } from '@/firebase/config'
 import { ref } from '@vue/reactivity'
+import {formatDistanceToNow} from "date-fns"
+import { computed, onUpdated } from '@vue/runtime-core'
 export default {
     setup(){
-        let messages = ref();
+        let messages = ref([]);
+        let msgBox=ref(null);
+
+        //auto scrolling feature
+        onUpdated(()=>{
+              msgBox.value.scrollTop = msgBox.value.scrollHeight;
+              
+            })
+        
+        let formatttedMessages = computed(()=>{
+          return messages.value.map((msg)=>{
+            let formatTime = formatDistanceToNow(msg.created_at.toDate());
+            return {...msg, created_at: formatTime};
+          })
+        })
+         
         db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
                 let results = [];
 
@@ -28,7 +45,8 @@ export default {
             messages.value = results;
 
         })
-        return {messages};
+        
+        return { formatttedMessages, msgBox};
     }
 }
 </script>
